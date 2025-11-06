6eb5c3c46e3e6bdd485667411fe64ace2df9bbe1
Jere Tolonen <tolonenjere0@gmail.com>
2025-11-06 14:56:02 +0200

    made animations and navbar for logged in users

--- frontend/includes/navbar/navbarUser.php
+++ frontend/includes/navbar/navbarUser.php
@@ -19,10 +19,15 @@
         </div>
     </div>
     <div class="mrg-10"></div>
-    <div class="user-navbar-open space-between col" style="display:none"id="userDropdown">
+    <div class="user-navbar-open space-between col" id="userDropdown">
         <div class="row space-between mrg-in-10">
             <a href="#" class="text-primary text-user-navbar-secondary" style="display:flex;align-self:center">Omat varaukset</a>
-            <img src="assets/icons/burger-menu-green.svg" style="height:24px;isplay:flex;align-self:center">
+            <img src="assets/icons/house-green.svg" style="height:24px;isplay:flex;align-self:center">
+        </div>
+        <div class="mrg-5"></div>
+        <div class="row space-between mrg-in-10">
+            <a href="#" class="text-primary text-user-navbar-secondary" style="display:flex;align-self:center">Kirjaudu ulos</a>
+            <img src="assets/icons/house-green.svg" style="height:24px;isplay:flex;align-self:center">
         </div>
     </div>
 </div>
@@ -30,27 +35,23 @@
     // variables for hidden nav and burger menu button
     const burgerMenuButton = document.getElementById("userBurgerMenu");
     const userDropdown = document.getElementById("userDropdown");
-
+    
     // function for burger menu opening and closing
     burgerMenuButton.addEventListener("click", function() {
         const isOpen = burgerMenuButton.classList.contains('rotate-clockwise-to-90');
         
         if (!isOpen) {
-            // rotate to 90deg = open menu
-            burgerMenuButton.classList.remove('rotate-clockwise-to-90-reverse');
-            void burgerMenuButton.offsetWidth;
-            burgerMenuButton.classList.add('rotate-clockwise-to-90');
             openBurgerMenu();
         } else {
-            // rotate back = close menu
-            burgerMenuButton.classList.remove('rotate-clockwise-to-90');
-            void burgerMenuButton.offsetWidth;
-            burgerMenuButton.classList.add('rotate-clockwise-to-90-reverse');
             closeBurgerMenu();
         }
     });
 
     function openBurgerMenu() {
+        // rotate to 90deg = open menu
+        burgerMenuButton.classList.remove('rotate-clockwise-to-90-reverse');
+        void burgerMenuButton.offsetWidth;
+        burgerMenuButton.classList.add('rotate-clockwise-to-90');
         userDropdown.classList.remove('burger-menu-close');
         void userDropdown.offsetWidth;
         userDropdown.classList.add('burger-menu-open');
@@ -58,6 +59,10 @@
     }
 
     function closeBurgerMenu() {
+        // rotate back = close menu
+        burgerMenuButton.classList.remove('rotate-clockwise-to-90');
+        void burgerMenuButton.offsetWidth;
+        burgerMenuButton.classList.add('rotate-clockwise-to-90-reverse');
         userDropdown.classList.remove('burger-menu-open');
         void userDropdown.offsetWidth;
         userDropdown.classList.add('burger-menu-close');
