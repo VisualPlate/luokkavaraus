fe14c1e3a5123fe03d3c42b4204a28447ec5ca32
aleksipurmonen <aleksi.purmonen@edu.sakky.fi>
2025-11-06 13:55:11 +0200

    Update navbar.php

--- frontend/includes/navbar/navbar.php
+++ frontend/includes/navbar/navbar.php
@@ -1,10 +1,5 @@
 <!--NAVBAR TO NON-LOGGED IN USERS AND THE MAIN PAGE-->
 
-<link rel="stylesheet" href="../../assets/css/config/general.css">
-<link rel="stylesheet" href="../../assets/css/config/theme.css">
-<link rel="stylesheet" href="../../assets/css/main.css">
-<link rel="stylesheet" href="../../assets/css/navbarTest.css">
-
 <div class = "navbar row space-between">
 
     <img src="../../assets/icons/house-green.svg" style="height: 100%">
