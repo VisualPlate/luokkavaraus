832b0be68cafea24c17adfd97facbd6f9b02d734
Justus Putkonen <justus.putkonen@gmail.com>
2025-11-07 13:19:21 +0200

    updated

--- frontend/index.php
+++ frontend/index.php
@@ -3,7 +3,20 @@
     require_once("includes/htmlHead/htmlHead.php");
     ?>
     <title>INDEX</title>
+    <link rel="stylesheet" href="index.css">
 </head>
 <body>
+    <?php
+    require_once("includes/navbar/navbar.php")
+    ?>
+    <div class="hero-bg">
+<div class="class-img">
+<img src="../assets/images/classroomimage.jpg" alt="">
+
+<h1 class = "text-primary">Paikallinen <br> luokkavaraus järjestelmnä</h1>
+
+</div>
+
+    </div>
 </body>
 </html>
