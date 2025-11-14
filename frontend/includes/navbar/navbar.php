807a70eb0295693e777f1736286ba3210e1b6e08
aleksipurmonen <aleksi.purmonen@edu.sakky.fi>
2025-11-14 11:03:09 +0200

    Update navbar.php

--- frontend/includes/navbar/navbar.php
+++ frontend/includes/navbar/navbar.php
@@ -3,9 +3,8 @@
 
     <img src="assets/icons/house-green.svg" style="height: 100%">
     <div class="row space-between">
-    <button class="navbar-btn  mrg-in-10" onclick="window.location.href=''">Varaa</button>
 
-    <button class="navbar-btn mrg-in-10" onclick="window.location.href='pages/searchPage.php'">hae</button>
+    <button class="navbar-btn mrg-in-10" onclick="window.location.href='pages/searchPage.php'">Varaa luokkahuone</button>
     </div>
 </div>
 
