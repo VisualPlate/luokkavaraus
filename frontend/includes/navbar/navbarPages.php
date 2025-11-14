e3932e1bc60ec55682da7baf244c486f6e664cb9
Jere Tolonen <tolonenjere0@gmail.com>
2025-11-14 11:05:46 +0200

    Create navbarPages.php

---
+++ frontend/includes/navbar/navbarPages.php
@@ -0,0 +1,10 @@
+<!--NAVBAR TO NON-LOGGED IN USERS AND THE MAIN PAGE-->
+<div class = "navbar row space-between">
+
+    <img src="../assets/icons/house-green.svg" style="height: 100%">
+    <div class="row space-between">
+
+    <button class="navbar-btn mrg-in-10" onclick="window.location.href='searchPage.php'">Varaa luokkahuone</button>
+    </div>
+</div>
+
